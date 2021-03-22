e53448699fe4cf03de7467876ab5ed4a877fcdc2
Ajaysinyh Zala <42064450+ajayzala08@users.noreply.github.com>
2021-03-22 10:43:23 +0530

    22-03-2021

---
+++ Controllers/clscfidController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace WebApplication3.Controllers
+{
+    [EnableCors(origins:"*",headers:"*",methods:"*")]
+    public class clscfidController : ApiController
+    {
+        public HttpResponseMessage get()
+        {
+            HttpResponseMessage response;
+            try
+            {
+                using (var db = new CompanyFormation_dbEntities())
+                {
+                    var cfid = db.cls_agree_tbl.OrderByDescending(x => x.cfid).FirstOrDefault().cfid.ToString();
+                    if (cfid != null && cfid.ToString() != "")
+                    {
+                        Dictionary<string, string> keyValues = new Dictionary<string, string>();
+                        keyValues.Add("cfid", cfid);
+                        response = Request.CreateResponse(HttpStatusCode.OK, keyValues);
+                    }
+                    else
+                    {
+                        response = Request.CreateResponse(HttpStatusCode.NotFound, "Not Found");
+                    }
+                    return response;
+                }
+            }
+            catch (Exception ex)
+            {
+                response = Request.CreateErrorResponse(HttpStatusCode.ExpectationFailed, "Exception");
+                return response;
+            }
+        }
+    }
+}
